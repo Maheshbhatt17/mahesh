d5b442f4b787fcc37d67b97c7cb604e63432df92
Maheshbhatt17 <38201196+Maheshbhatt17@users.noreply.github.com>
2020-02-17 15:52:14 +0530

    Entity framework database first workflow
    This file is program.cs file for Entity framework database first workflow. That defines the value added into the table attributes by the code.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databasefirstdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabasefirstdemoEntities();
+            var post = new Post()
+            {
+                Body = "hello",
+                DatePublished = DateTime.Now,
+                Title = "python",
+                Postid=2
+
+
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
